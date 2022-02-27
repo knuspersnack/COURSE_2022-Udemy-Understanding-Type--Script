2bfda34e56132183bea9da0a1957954daa0fdab3
knuspersnack <geo.d@posteo.de>
2022-02-27 23:43:54 +0100

    Classes 1

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,37 @@
-const person: {
-  name: string,
-  age: number,
-  hobbies: string[];
-  role: [number, string, string]
-} = {
-  name: "test",
-  age: 21,
-  hobbies: ['sports', 'cooking'],
-  role: [2, 'author', 'georg']
+class Department {
+  //private name: string;
+  private employees: string[] = [];
+
+  constructor(private id:string, private name: string) {
+  }
+
+  describe() {
+    console.log('Department: name-> ' + this.name + ', id -> ' + this.id);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-const test = generateError("test");
-console.log(test)
-
-function generateError(message:string) {
-  console.log("test23")
+class ITDepartment extends Department{
+  constructor(id: string, public admins: string[]) {
+    super(id, 'IT');
+  }
 }
+
+
+
+const accounting = new ITDepartment('d1', ['IT Nerd']);
+console.log(accounting);
+accounting.describe();
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Menu');
+accounting.printEmployeeInformation();
+console.log(accounting.admins)
