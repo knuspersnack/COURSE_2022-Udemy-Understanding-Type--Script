c33b74f65d517c41376bfc46adab1f8e55b5a267
knuspersnack <geo.d@posteo.de>
2022-03-12 15:10:05 +0100

    Singelton

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,9 @@ abstract class Department {
   //private name: string;
   private employees: string[] = [];
 
-  constructor(private id:string, private name: string) {
-  }
+  constructor(private id: string, private name: string) {}
 
-  abstract describe(this:Department):void;
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -17,30 +16,39 @@ abstract class Department {
   }
 }
 
-class ITDepartment extends Department{
-  constructor(id: string, public admins: string[]) {
+class ITDepartment extends Department {
+  private constructor(id: string, public admins: string[]) {
     super(id, 'IT');
   }
+
+  private static instance: ITDepartment;
+
+  static getIntance() {
+    if(!ITDepartment.instance) {
+      ITDepartment.instance = new ITDepartment("Test",['IT Nerd']);
+    }
+    return ITDepartment.instance;
+  }
+
   describe() {
-    console.log("Described from ITDepartment")
+    console.log('Described from ITDepartment');
   }
 }
 
-class MathDepartment extends Department{
+class MathDepartment extends Department {
   constructor(id: string, public admins: string[]) {
     super(id, 'Math');
   }
   describe() {
-    console.log("Described from Math")
+    console.log('Described from Math');
   }
 }
 
-
-const accounting = new ITDepartment('d1', ['IT Nerd']);
+const accounting = ITDepartment.getIntance();
 console.log(accounting);
 accounting.describe();
 
 accounting.addEmployee('Max');
 accounting.addEmployee('Menu');
 accounting.printEmployeeInformation();
-console.log(accounting.admins)
+console.log(accounting.admins);
