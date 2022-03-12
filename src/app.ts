20a232679ed486ac1c0af432fe7ca3ebc4dc49ef
knuspersnack <geo.d@posteo.de>
2022-03-12 19:20:59 +0100

    Type Guards

--- src/app.ts
+++ src/app.ts
@@ -13,5 +13,47 @@ type ElevatedEmployee = Admin & Employee;
 const e1: ElevatedEmployee = {
     name: 'Max',
     privileges: ['create-server'],
-    startDate: new Date();
+    startDate: new Date()
 }
+
+type Combinable = string | number;
+type Numberic = number | boolean;
+
+type Universal = Combinable & Numberic;
+
+function add(a: Combinable, b:Combinable) {
+    if(typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString()
+    }
+
+    return a + b;
+}
+
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+
+    loadCargo(amount: Number) {
+        console.log('Loading cargo...', amount);
+    }
+}
+
+type Vehicle = Car | Truck;
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    if(vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
