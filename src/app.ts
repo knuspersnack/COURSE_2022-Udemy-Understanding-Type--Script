c3feb7d860c570be718b7c5a73f2fa2e0ebc07db
knuspersnack <geo.d@posteo.de>
2022-03-13 21:39:54 +0100

    discriminated unions

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,48 @@
 type Admin = {
-    name: string;
-    privileges: string[];
-}
+  name: string;
+  privileges: string[];
+};
 
 type Employee = {
-    name: string;
-    startDate: Date;
-}
+  name: string;
+  startDate: Date;
+};
 
 type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
-    name: 'Max',
-    privileges: ['create-server'],
-    startDate: new Date()
-}
+  name: 'Max',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
 
 type Combinable = string | number;
 type Numberic = number | boolean;
 
 type Universal = Combinable & Numberic;
 
-function add(a: Combinable, b:Combinable) {
-    if(typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString()
-    }
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
 
-    return a + b;
+  return a + b;
 }
 
 class Car {
-    drive() {
-        console.log('Driving...');
-    }
+  drive() {
+    console.log('Driving...');
+  }
 }
 
 class Truck {
-    drive() {
-        console.log('Driving a truck...');
-    }
+  drive() {
+    console.log('Driving a truck...');
+  }
 
-    loadCargo(amount: Number) {
-        console.log('Loading cargo...', amount);
-    }
+  loadCargo(amount: Number) {
+    console.log('Loading cargo...', amount);
+  }
 }
 
 type Vehicle = Car | Truck;
@@ -50,10 +50,36 @@ const v1 = new Car();
 const v2 = new Truck();
 
 function useVehicle(vehicle: Vehicle) {
-    vehicle.drive();
-    if(vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
 }
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed = 0;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+  }
+  console.log('Moving with speed : ' + speed);
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 10});
